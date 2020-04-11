2215d1335a8468f29bd3f127ca8832c90f1dd03a
stas_malikov <stas.malikov75@gmail.com>
2020-04-11 11:49:45 +0300

    стартовая страница

--- Проект/front/src/app/homepage/homepage.component.ts
+++ Проект/front/src/app/homepage/homepage.component.ts
@@ -9,7 +9,16 @@ export class HomepageComponent implements OnInit {
 
   constructor() { }
 
+  football: string;
+  hockey: string;
+  basketball: string;
+  volleyball: string;
+
   ngOnInit() {
+    this.football = 'assets/img/football.jpg';
+    this.hockey = 'assets/img/hockey.jpg';
+    this.basketball = 'assets/img/basketball.jpg';
+    this.volleyball = 'assets/img/volleyball.jpg';
   }
 
 }
