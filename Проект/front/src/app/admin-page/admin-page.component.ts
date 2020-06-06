e1591da075a0ef4a2ab38d872f37b21ae949c1bf
stas_malikov <stas.malikov75@gmail.com>
2020-06-06 23:43:01 +0300

    [https://trello.com/c/i6PkFSos] Поиск по пользователям в администрировании

--- Проект/front/src/app/admin-page/admin-page.component.ts
+++ Проект/front/src/app/admin-page/admin-page.component.ts
@@ -7,6 +7,7 @@ import {HttpClient} from '@angular/common/http';
 import {EventService} from '../services/event-service';
 import {Router} from '@angular/router';
 import {MiniUser} from '../models/user/MiniUser';
+import {FindUser} from '../models/user/FindUser';
 
 @Component({
   selector: 'app-admin-page',
@@ -17,10 +18,16 @@ export class AdminPageComponent implements OnInit {
   users: StatusUser[];
   usersCount = 0;
   userName = '';
+  visibleSearchUser = false;
+  userFound: StatusUser;
+  notFoundUser = false;
 
   moderators: StatusUser[];
   moderatorCount = 0;
   moderatorName = '';
+  visibleSearchModerator = false;
+  notFoundModerator = false;
+  moderatorFound: StatusUser;
 
   constructor(private auth: AuthService, private http: HttpClient,
               private eventService: EventService, private router: Router) { }
@@ -41,6 +48,9 @@ export class AdminPageComponent implements OnInit {
         } else {
           this.usersCount = this.users.length;
         }
+
+        this.visibleSearchUser = false;
+        this.notFoundUser = false;
       }, error => {
         alert('Упс, ошибка');
       });
@@ -63,19 +73,24 @@ export class AdminPageComponent implements OnInit {
   }
 
   findUsers() {
-    // this.http.post( environment.apiUrl + '/api/v1/admin/get_all_users' , '', {
-    //   headers: {Authorization: 'TSToken_' + localStorage.getItem('auth_token')}
-    // })
-    //   .subscribe((resp: StatusUser[]) => {
-    //     this.users = resp;
-    //     if (typeof this.users === undefined) {
-    //       this.usersCount = 0;
-    //     } else {
-    //       this.usersCount = this.users.length;
-    //     }
-    //   }, error => {
-    //     alert('Упс, ошибка');
-    //   });
+    const body: FindUser = {
+      info: this.userName
+    };
+
+    this.http.post( environment.apiUrl + '/api/v1/admin/get_user_by_name', body, {
+      headers: {Authorization: 'TSToken_' + localStorage.getItem('auth_token')}
+    })
+      .subscribe((resp: StatusUser) => {
+        if (resp !== undefined && resp !== null) {
+          this.userFound = resp;
+          this.visibleSearchUser = true;
+        } else {
+          this.visibleSearchUser = false;
+          this.notFoundUser = true;
+        }
+      }, error => {
+        alert('Упс, ошибка');
+      });
   }
 
   addModerator(statusUser: StatusUser) {
