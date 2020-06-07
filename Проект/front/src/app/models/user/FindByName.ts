308594878e2853db37cafd12c1f3979ea4828d4b
stas_malikov <stas.malikov75@gmail.com>
2020-06-07 13:12:07 +0300

    [https://trello.com/c/i6PkFSos] Поиск в модерации

---
+++ Проект/front/src/app/models/user/FindByName.ts
@@ -0,0 +1,3 @@
+export class FindByName {
+  name: string;
+}
--- Проект/front/src/app/moder-page/moder-page.component.ts
+++ Проект/front/src/app/moder-page/moder-page.component.ts
@@ -8,6 +8,8 @@ import {EventService} from '../services/event-service';
 import {Router} from '@angular/router';
 import {EventStatus} from '../models/event/EventStatus';
 import {UserStatus} from '../models/user/UserStatus';
+import {FindUser} from '../models/user/FindUser';
+import {FindByName} from '../models/user/FindByName';
 
 @Component({
   selector: 'app-moder-page',
@@ -21,6 +23,14 @@ export class ModerPageComponent implements OnInit {
   bannedEvents: FullEvent[];
   bannedEventsCount = 0;
 
+  userName = '';
+  foundUser: StatusUser;
+  seeFoundUser = false;
+  foundUserNotification = false;
+
+  eventName = '';
+  foundEvents: FullEvent[];
+  foundEventsCount = 0;
 
   constructor(private auth: AuthService, private http: HttpClient,
               private eventService: EventService, private router: Router) { }
@@ -99,6 +109,22 @@ export class ModerPageComponent implements OnInit {
       });
   }
 
+  lockEvent(fullEvent: FullEvent) {
+    const body: EventStatus = {
+      eventID: fullEvent.eventID,
+      status: 'BANNED'
+    };
+
+    this.http.post( environment.apiUrl + '/api/v1/moder/set_eventStatus', body, {
+      headers: {Authorization: 'TSToken_' + localStorage.getItem('auth_token')}
+    })
+      .subscribe((resp: any) => {
+        this.getBannedEvents();
+      }, error => {
+        alert('Упс, ошибка');
+      });
+  }
+
   unlockUser(user: StatusUser) {
     const body: UserStatus = {
       userName: user.login,
@@ -114,5 +140,77 @@ export class ModerPageComponent implements OnInit {
         alert('Упс, ошибка');
       });
   }
+
+  lockUser(user: StatusUser) {
+    const body: UserStatus = {
+      userName: user.login,
+      status: 'BANNED'
+    };
+
+    this.http.post( environment.apiUrl + '/api/v1/moder/set_userStatus', body, {
+      headers: {Authorization: 'TSToken_' + localStorage.getItem('auth_token')}
+    })
+      .subscribe((resp: any) => {
+        this.getBannedUsers();
+      }, error => {
+        alert('Упс, ошибка');
+      });
+  }
+
+  findUser() {
+    const body: FindByName = {
+      name: this.userName
+    };
+
+    this.http.post( environment.apiUrl + '/api/v1/moder/usersByName', body, {
+      headers: {Authorization: 'TSToken_' + localStorage.getItem('auth_token')}
+    })
+      .subscribe((resp: StatusUser) => {
+        if (resp !== undefined && resp !== null) {
+          this.foundUser = resp;
+          this.seeFoundUser = true;
+          this.foundUserNotification = false;
+        } else {
+          this.seeFoundUser = false;
+          this.foundUserNotification = true;
+        }
+      }, error => {
+        alert('Упс, ошибка');
+      });
+  }
+
+  findEvent() {
+    const body: FindByName = {
+      name: this.eventName
+    };
+
+    this.http.post( environment.apiUrl + '/api/v1/moder/eventsByName', body, {
+      headers: {Authorization: 'TSToken_' + localStorage.getItem('auth_token')}
+    })
+      .subscribe((resp: FullEvent[]) => {
+        this.foundEvents = resp;
+        if (typeof this.foundEvents === undefined) {
+          this.foundEventsCount = 0;
+        } else {
+          this.foundEventsCount = this.foundEvents.length;
+        }
+      }, error => {
+        alert('Упс, ошибка');
+      });
+  }
+
+  public  isBannedUser(user: StatusUser): boolean {
+    if (user.status === 'ACTIVE') {
+      return false;
+    }
+    return true;
+  }
+
+  // public  isBannedEvent(event: FullEvent): boolean {
+  //   if (event. === 'ACTIVE') {
+  //     return false;
+  //   }
+  //   return true;
+  // }
 
 }
